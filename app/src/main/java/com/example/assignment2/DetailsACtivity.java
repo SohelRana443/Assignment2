b23a345657d410d45fe5974a2c297e7c9179e6a1
SohelRana443 <sohel.rana1.cse@ulab.edu.bd>
2021-12-28 21:21:28 +0600

    details

--- app/src/main/java/com/example/assignment2/DetailsACtivity.java
+++ app/src/main/java/com/example/assignment2/DetailsACtivity.java
@@ -11,10 +11,15 @@ import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsACtivity extends AppCompatActivity {
 
     Result result;
+    ImageView img1,img2;
+    TextView tv1, tv2, tv3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,19 @@ public class DetailsACtivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         result = (Result) getIntent().getSerializableExtra("result");
+
+        img1=(ImageView)findViewById(R.id.back_drop_path);
+        img2=(ImageView)findViewById(R.id.poster_path);
+        tv1=(TextView) findViewById(R.id.original_title);
+        tv2=(TextView) findViewById(R.id.vote_count);
+        tv3=(TextView) findViewById(R.id.over_view);
+
+
+        img1.setImageResource(getIntent().getIntExtra("BackdropPath",defaultValue:0))
+        img2.setImageResource(getIntent().getIntExtra("PosterPath",defaultValue:0));
+        tv1.setText(getIntent().getStringExtra("OriginalTitle"));
+        tv1.setText(getIntent().getStringExtra("VoteCount"));
+        tv1.setText(getIntent().getStringExtra("Overview"));
 
         }
 
